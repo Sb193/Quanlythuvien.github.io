d518a3f2f6604e85bdaf731ec51f6516f8a869c1
Your Name <you@example.com>
2023-03-16 11:33:55 +0700

    ok

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,5 +2,5 @@ var login_button = document.getElementById("login_button").addEventListener("cli
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     console.log(username , password);
-    let formlogin = document.getElementById("login__form").style.display = "none";
+    window.open("./index.html");
 });
