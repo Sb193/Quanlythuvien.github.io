75777bed7a48e05900403c83509c0e22af19ee5b
vlong20 <124040142+vlong20@users.noreply.github.com>
2023-03-19 22:06:07 +0700

    [03/19 22:05PM] addbook form was completed

--- assets/js/addbook.js
+++ assets/js/addbook.js
@@ -1,3 +1,11 @@
-function display(){
-    document.getElementsByClassName("book__add__submit__pop-up").style.display = "block";
+function back_to_index(){
+    window.location.href = "./index.html";
 }
+
+function show(){
+    document.getElementById("book__add__submit__pop-up").style.display = "block";
+}
+
+/*
+Thêm dữ liệu sách vào csdl ở đây nhé
+*/
