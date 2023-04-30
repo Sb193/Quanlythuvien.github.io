41bf38e444a0078b79b5c012d287f2a18eb31bb1
Sb193 <103234991+Sb193@users.noreply.github.com>
2023-04-30 14:42:05 +0700

    Delete assets directory
    xoas

--- assets/js/login.js
+++
@@ -1,6 +0,0 @@
-var login_button = document.getElementById("login_button").addEventListener("click", function(){
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    console.log(username , password);
-    window.open("./index.html");
-});
